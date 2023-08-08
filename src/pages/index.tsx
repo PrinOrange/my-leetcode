53033d3e9d60f05fbec1bd6ea96f2036686c7ff4
PrinOrange <prinorange@outlook.com>
2023-08-08 14:50:49 +0800

    fix the broken link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/notes/intro">
             📕 Getting Start 🖊
           </Link>
         </div>
